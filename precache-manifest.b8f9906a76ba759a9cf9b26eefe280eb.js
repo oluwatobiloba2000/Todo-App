07e342599d86117aca8a98ee7142303d24408644
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-10-24 04:34:11 +0100

    Updates

---
+++ precache-manifest.b8f9906a76ba759a9cf9b26eefe280eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8e7a5a713339540fa6c64aa10e1705",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "190d724e411ca1e666f1",
+    "url": "/Todo-App/static/css/main.95554331.chunk.css"
+  },
+  {
+    "revision": "5601f779d4c5968ae24e",
+    "url": "/Todo-App/static/js/2.872699c0.chunk.js"
+  },
+  {
+    "revision": "190d724e411ca1e666f1",
+    "url": "/Todo-App/static/js/main.e5db2a30.chunk.js"
+  },
+  {
+    "revision": "79bbe39763cc6294826e",
+    "url": "/Todo-App/static/js/runtime-main.bc47dec1.js"
+  },
+  {
+    "revision": "ee744409591b015182f9c3a0fdaba9f5",
+    "url": "/Todo-App/static/media/todo-pics.ee744409.png"
+  }
+]);
